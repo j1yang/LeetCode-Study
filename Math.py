bd1de4a4e30858924bced7a85dadfe1285b77e2d
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-11-04 19:09:03 -0400

    two math problems

--- Math.py
+++ Math.py
@@ -2,3 +2,30 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return False if x < 0 else str(x) == str(x)[::-1]
+
+# 69
+    def mySqrt(self, x: int) -> int:
+        return math.floor(sqrt(x))
+
+# 67
+
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] = digits[-1] + 1
+
+        if digits[-1] != 10:
+            return digits
+
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] == 10:
+                if i != 0:
+                    digits[i] = 0
+                    digits[i-1] = digits[i-1] + 1
+            elif digits[i] != 10:
+                return digits
+
+        if digits[0] == 10:
+            digits[0] = 0
+            digits.insert(0, 1)
+            return digits
+
+        return digits
