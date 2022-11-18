bfff49146fe0f4a7a15e3135a091fbd917f6e472
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-11-17 19:28:00 -0500

    array

--- array.py
+++ array.py
@@ -2,3 +2,7 @@ class Solution:
   #136
   def singleNumber(self, nums: List[int]) -> int:
     return 2*(sum(set(nums)))-sum(nums)
+  #169
+  def majorityElement(self, nums: List[int]) -> int:
+    nums.sort()
+    return nums[len(nums)//2]
