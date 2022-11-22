2291ec1f4acc62b060f2411d0e0fd84fda7b72db
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-11-21 22:21:56 -0500

    array

--- array.py
+++ array.py
@@ -12,3 +12,10 @@ class Solution:
         return False
     else:
         return True
+  #35
+  def searchInsert(self, nums: List[int], target: int) -> int:
+        nSet = set()
+        for i in range(0, len(nums)):
+            if target - nums[i] <= 0:
+                return i
+        return len(nums)
